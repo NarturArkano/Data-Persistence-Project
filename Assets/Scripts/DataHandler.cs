9d28e976af33b0a215ec9cb4c7093133ab038e4b
Steven Adler <NarturArkano@users.noreply.github.com>
2023-11-28 11:02:15 -0700

    Beginning data persistence
    Set up saving and loading.
    No calls yet from other scripts.

---
+++ Assets/Scripts/DataHandler.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DataHandler : MonoBehaviour
+{
+    public static DataHandler Instance;
+
+    public string playerName;
+    public int highScore;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public string playerName;
+        public int highScore;
+    }
+
+    public void SaveScore()
+    {
+        SaveData data = new SaveData();
+        data.playerName = playerName;
+        data.highScore = highScore;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadScore()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            playerName = data.playerName;
+            highScore = data.highScore;
+        }
+    }
+}
